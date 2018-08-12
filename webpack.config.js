90fe6e95cc99dd1cbd90d50f041f7674a7e3b44b
Mythreyi M <Mythreyi.M@cognizant.com>
2018-08-12 10:48:01 +1000

    initial commit | basic

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+  entry: [path.resolve(process.cwd(), 'vendor/index.js')],
+
+  output: {
+    filename: 'vendor.js',
+    path: path.resolve(process.cwd(), 'lib'),
+    libraryTarget: 'commonjs2',
+  },
+
+  mode: 'production',
+
+  module: {
+    rules: [
+      {
+        exclude: /node_modules\/(?!(dom7|swiper)\/).*/,
+        test: /\.(js|jsx)$/,
+        loader: 'babel-loader',
+        options: {
+          babelrc: false,
+          presets: [['env', {
+            'modules': false,
+          }]],
+        },
+      },
+    ],
+  },
+};
